feae0ab04440475c64d46d61c95e7fe14c8d5b73
Michelle Ranly <meranly8@gmail.com>
2020-01-20 21:46:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,20 @@
 def display_board(board)
-  puts " #{board[0]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board_index)
+end
+
+def position_taken(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
