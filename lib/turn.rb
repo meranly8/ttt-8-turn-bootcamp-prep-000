49661bce3fb81a2382592db3cc8a02aad46b5035
Michelle Ranly <meranly8@gmail.com>
2020-01-20 21:56:15 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ def input_index(user_input)
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board_index)
+  index.between?(0,8) && !position_taken?(board, index)
 end
 
 def position_taken(board, index)
