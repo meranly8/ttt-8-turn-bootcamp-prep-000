3977d0486acce69f28ec75d90f8796f144e37857
Michelle Ranly <meranly8@gmail.com>
2020-01-20 21:58:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
-def position_taken(board, index)
+def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
